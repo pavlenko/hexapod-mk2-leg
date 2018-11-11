99739e477af0f4413a5a6ee9ed22917f7afe4b8a
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-11-11 15:47:28 +0200

    add timer usage example

---
+++ lib/Timer/examples/timer.cpp
@@ -0,0 +1,36 @@
+#include <Timer.h>
+
+void timer1InterruptHandler() {
+    // Do something on interrupt
+}
+
+int main() {
+    // Most timers has API as below, now covered timers 0...5
+
+    // Set clock source
+    Timer1.setClockSource(TIMER_CLOCK_DIVIDE_BY_8);
+
+    // Set count mode
+    Timer1.setCountMode(TIMER_16BIT_COUNT_NORMAL);
+
+    // Set output mode for channels A/B/C
+    Timer1.setOutputModeA(TIMER_OUTPUT_TOGGLE);
+
+    // Set interrupt handler as pointer to function
+    Timer1.setInterruptHandler(TIMER_INTERRUPT_OVERFLOW, timer1InterruptHandler);
+
+    // Set interrupt handler as lambda function
+    Timer1.setInterruptHandler(TIMER_INTERRUPT_COMPARE_MATCH_A, [](){
+        // Do something on compare match
+    });
+
+    // Set initial counter value
+    *Timer1.TCNTn = 0x0F;
+
+    // Set compare match value
+    *Timer1.OCRnA = 0xF0;
+
+    // Enable interrupts
+    Timer0.setInterruptEnabled(TIMER_INTERRUPT_OVERFLOW, true);
+    Timer0.setInterruptEnabled(TIMER_INTERRUPT_COMPARE_MATCH_A, true);
+}
