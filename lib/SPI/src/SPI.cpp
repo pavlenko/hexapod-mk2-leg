d8d8661ffa94fc1064c6be42dbe89f0746f2ab5e
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-11-24 13:55:35 +0200

    update SPI code

--- lib/SPI/src/SPI.cpp
+++ lib/SPI/src/SPI.cpp
@@ -1,1 +1,23 @@
 #include "SPI.h"
+
+#include <avr/io.h>
+#include <stdint.h>
+
+void SPIClass::initialize(SPIConnection_t connection, SPISettings_t settings) {
+    // Configure SPI pins for master mode as output
+    *(connection.DDR) |= _BV(connection.MOSI) | _BV(connection.SCK);
+
+    // Configure SPI settings
+    SPCR = (settings.dataOrder << DORD) | (settings.clockMode << CPHA) | (settings.clockSource << SPR0);
+
+    // Set SPI master mode
+    SPCR |= _BV(MSTR);
+}
+
+void SPIClass::initialize(SPIConnection_t connection) {
+    // Configure SPI pins for slave mode
+    *(connection.DDR) |= _BV(connection.MISO);
+
+    // Set SPI master mode
+    SPCR &= ~_BV(MSTR);
+}
--- lib/SPI/src/SPI.h
+++ lib/SPI/src/SPI.h
@@ -3,18 +3,11 @@
 
 #include <avr/io.h>
 #include <stdint.h>
+#include <stddef.h>
 
 // ATMega16/32: SS=PB4, SCK=PB7, MOSI=PB5, MISO=PB6
 // ATMega8: SS=PB2, SCK=PB5, MOSI=PB3, MISO=PB4
 // ATMega64(0)/128(0/1)/256(0/1): SS=PB0, SCK=PB1, MOSI=PB2, MISO=PB3
-
-typedef struct {
-    volatile uint8_t *PORT;
-    int8_t SS: 4;
-    int8_t SCK: 4;
-    int8_t MOSI: 4;
-    int8_t MISO: 4;
-} SPIConnection;
 
 enum SPIDataOrder {
     SPI_DATA_ORDER_MSB,
@@ -43,29 +36,158 @@ enum SPIClockSource {
     SPI_CLOCK_DIVIDE_BY_128,
 };
 
+enum SPIClockMode {
+    SPI_CLOCK_SAMPLE_RISING_NORMAL,
+    SPI_CLOCK_SAMPLE_FALLING_NORMAL,
+    SPI_CLOCK_SAMPLE_RISING_INVERTED,
+    SPI_CLOCK_SAMPLE_FALLING_INVERTED,
+};
+
+//TODO for slave pass {port, ddr, mosi pin, miso pin, sck pin, ss pin}
+//TODO for master pass {port, ddr, mosi pin, miso pin, sck pin}
+//TODO master multiple slave support: for each slave pass SS pin {port, ddr, pinnum}
+
+class SPI2;
+
+class SPIConnection {
+    friend SPI2;
+private:
+    volatile uint8_t *PORT;
+    volatile uint8_t *DDR;
+};
+
+class SPIMasterConnection: public SPIConnection {
+public:
+    SPIMasterConnection();
+};
+
+class SPISlaveConnection: public SPIConnection {
+public:
+    SPISlaveConnection();
+};
+
+typedef struct {
+    volatile uint8_t *port;
+    volatile uint8_t *ddr;
+} SPIMasterConnection_t;
+
+typedef struct {
+    volatile uint8_t *port;
+    volatile uint8_t *ddr;
+} SPISlaveConnection_t;
+
+typedef struct {
+    volatile uint8_t *port;
+    volatile uint8_t *ddr;
+    uint8_t pin;
+} SPIChipSelect_t;
+
+class SPIChipSelect {
+    friend SPI2;
+private:
+    volatile uint8_t *port;
+    volatile uint8_t *ddr;
+    uint8_t pin;
+};
+
+class SPI2 {
+private:
+    uint8_t connection;
+    SPIChipSelect *slave;
+public:
+    /**
+     * Initialize SPI as slave
+     *
+     * @param connection
+     */
+    void initialize(SPISlaveConnection connection) {
+        *(connection.DDR) |= _BV(connection.MISO);
+
+        // Set SPI as slave
+        SPCR &= ~_BV(MSTR);
+    }
+
+    /**
+     * Enable SPI and select slave device
+     *
+     * @param slave
+     */
+    void start(SPIChipSelect &slave) {
+        this->slave = &slave;
+
+        if (this->slave) {
+            // Set pin as output
+            *(this->slave->ddr) |= _BV(this->slave->pin);
+
+            // Set pin low to select slave
+            *(this->slave->port) &= ~_BV(this->slave->pin);
+        }
+
+        // Enable SPI
+        SPCR |= _BV(SPE);
+    }
+
+    /**
+     * Transmit one byte
+     *
+     * @param data
+     * @return
+     */
+    uint8_t transmit(uint8_t data) {
+        // Store data to transmit buffer
+        SPDR = data;
+
+        /**
+         * The following NOP introduces a small delay that can prevent the wait loop form iterating when running at the
+         * maximum speed.
+         * This gives about 10% more speed, even if it seems counter-intuitive.
+         * At lower speeds it is unnoticed.
+         */
+        asm volatile("nop");
+
+        // Wait while transmission completed
+        while (!(SPSR & _BV(SPIF)));
+
+        // Return received data
+        return SPDR;
+    }
+
+    /**
+     * Disable SPI and disconnect from slave device
+     */
+    void end() {
+        // Disable SPI
+        SPCR &= ~_BV(SPE);
+
+        if (this->slave) {
+            // Set pin high to release slave
+            *(this->slave->port) |= _BV(this->slave->pin);
+        }
+    }
+};
+
+
+typedef struct {
+    volatile uint8_t *DDR;
+    int8_t SS: 4;
+    int8_t SCK: 4;
+    int8_t MOSI: 4;
+    int8_t MISO: 4;
+} SPIConnection_t;
+
+typedef struct {
+    SPIClockSource clockSource;
+    SPIClockMode clockMode;
+    SPIDataOrder dataOrder;
+} SPISettings_t;
+
 class SPIClass {
 private:
-    SPIConnection connection;
+    SPIConnection_t connection;
 public:
-    void initialize(SPIConnection connection) {
-        this->connection = connection;
-    }
-    void setDataOrder(SPIDataOrder dataOrder) {
-        SPCR = (uint8_t) ((SPCR & _BV(DORD)) | (dataOrder << DORD));
-    }
-    void setMode(SPIMode mode) {
-        SPCR = (uint8_t) ((SPCR & _BV(MSTR)) | (mode << MSTR));
-    }
-    void setClockPolarity(SPIClockPolarity polarity) {
-        SPCR = (uint8_t) ((SPCR & _BV(CPOL)) | (polarity << CPOL));
-    }
-    void setClockPhase(SPIClockPhase phase) {
-        SPCR = (uint8_t) ((SPCR & _BV(CPHA)) | (phase << CPHA));
-    }
-    void setClockSource(SPIClockSource clockSource, bool x2) {
-        SPCR = (uint8_t) ((SPCR & 0x11111100) | clockSource);
-        SPSR = (uint8_t) ((SPSR & ~_BV(SPI2X)) | x2);
-    }
+    void initialize(SPIConnection_t connection, SPISettings_t settings);
+    void initialize(SPIConnection_t connection);
+    //TODO other transfer methods
     uint8_t transfer(uint8_t data) {
         SPDR = data;
 
