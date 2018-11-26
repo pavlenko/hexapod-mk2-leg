81231ffde7d6b445e828553e3cf7843b6ffc24d4
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-11-26 08:47:14 +0200

    add lib to draw in lcd independent from driver

---
+++ lib/LCD/src/LCD.cpp
@@ -0,0 +1,37 @@
+#include "LCD.h"
+
+LCD::LCD(uint8_t *buffer, uint8_t w, uint8_t h, void (*draw)()) {
+//TODO not yet implemented
+}
+
+void LCD::clear() {
+//TODO not yet implemented
+}
+
+void LCD::string(const char *string, uint8_t x, uint8_t y) {
+//TODO not yet implemented
+}
+
+void LCD::pixel(uint8_t x, uint8_t y) {
+//TODO not yet implemented
+}
+
+void LCD::line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) {
+//TODO not yet implemented
+}
+
+void LCD::rectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) {
+//TODO not yet implemented
+}
+
+void LCD::circle(uint8_t x, uint8_t y, uint8_t radius) {
+//TODO not yet implemented
+}
+
+void LCD::bitmap(uint8_t *bitmap, uint8_t x, uint8_t y, uint8_t w, uint8_t h) {
+//TODO not yet implemented
+}
+
+void LCD::flush() {
+//TODO not yet implemented
+}
---
+++ lib/LCD/src/LCD.h
@@ -0,0 +1,91 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include <stdint.h>
+
+class LCD {
+private:
+    uint8_t *_buffer;
+    uint8_t _w;
+    uint8_t _h;
+    void (*_draw)();
+public:
+    /**
+     * Initialize LCD buffer, size and draw callback
+     *
+     * @param buffer
+     * @param w
+     * @param h
+     * @param draw
+     */
+    LCD(uint8_t *buffer, uint8_t w, uint8_t h, void (*draw)());
+
+    /**
+     * Clear buffer to initial state
+     */
+    void clear();
+
+    /**
+     * Draw string
+     *
+     * @param string
+     * @param x
+     * @param y
+     */
+    void string(const char *string, uint8_t x, uint8_t y);
+
+    /**
+     * Draw single pixel
+     *
+     * @param x
+     * @param y
+     */
+    void pixel(uint8_t x, uint8_t y);
+
+    /**
+     * Draw line
+     *
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     */
+    void line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+
+    /**
+     * Draw rectangle
+     *
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     */
+    void rectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+
+    /**
+     * Draw circle
+     *
+     * @param x
+     * @param y
+     * @param radius
+     */
+    void circle(uint8_t x, uint8_t y, uint8_t radius);
+
+    /**
+     * Draw bitmap
+     *
+     * @param bitmap
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     */
+    void bitmap(uint8_t *bitmap, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
+
+    /**
+     * Send buffer to display via draw callback
+     */
+    void flush();
+};
+
+#endif //LCD_H
